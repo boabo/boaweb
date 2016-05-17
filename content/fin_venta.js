a47db18e73230a3f12bb273a34e51dceb6b42d0b
favio figueroa <fabiofigueroap@gmail.com>
2016-05-17 15:14:17 -0400

    js iniciar ui sections

--- content/fin_venta.js
+++ content/fin_venta.js
@@ -3,11 +3,20 @@
  */
 (function ($) {
 
+
+    $(document).on('ready', function () {
+
+        initialize_header(true);
+        initialize_ui_sections({anchor_section_headers: false});
+
+
+        todayStr = formatCompactDate(new Date()); // today
+    });
     
     fin_venta = {
 
         invoce : function (p1,p2) {
-
+            //creacion dinamica de un formulario con dos parametros
             var newForm = $('<form>', {
                 'action': 'http://www.dd.com/pdf',
                 'target': '_blank'
@@ -21,7 +30,7 @@
                 'type': 'hidden'
             }));
 
-            newForm.appendTo("body").submit();
+            newForm.appendTo("body").submit(); // esta forma aseguramos que en firefox funcione y en los demas
 
             // console.log(newForm);
             // newForm.submit();
