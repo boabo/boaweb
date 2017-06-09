e096c8662be4dcb74bec62ef7d05ac772680ff0c
faviofigueroa <fabiofigueroap@gmail.com>
2017-06-09 11:12:55 -0400

    validacion form

--- content/reserva_vuelos.js
+++ content/reserva_vuelos.js
@@ -147,6 +147,19 @@ var menor_tarifa_ida = 0;
 var menor_tarifa_vuelta = 0;
 
 var reserva_redirect = true;
+
+
+var validacion_ = {
+    nit: {
+        numericality: {
+            onlyInteger: true,
+            greaterThan:0,
+            message:"Solo numero campeon",
+        },
+
+    }
+};
+
 // ---------------------= =---------------------
 /********************************************************* 
  ********************** UI HANDLERS **********************
@@ -154,7 +167,7 @@ var reserva_redirect = true;
 $(document).on('ready',function()
 {
 
-    (function(a){a.fn.validCampos=function(b){a(this).on({keypress:function(a){var c=a.which,d=a.keyCode,e=String.fromCharCode(c).toLowerCase(),f=b;(-1!=f.indexOf(e)||9==d||37!=c&&37==d||39==d&&39!=c||8==d||46==d&&46!=c)&&161!=c||a.preventDefault()}})}})(jQuery);
+    //(function(a){a.fn.validCampos=function(b){a(this).on({keypress:function(a){var c=a.which,d=a.keyCode,e=String.fromCharCode(c).toLowerCase(),f=b;(-1!=f.indexOf(e)||9==d||37!=c&&37==d||39==d&&39!=c||8==d||46==d&&46!=c)&&161!=c||a.preventDefault()}})}})(jQuery);
 
 
     $('#razon_social').validCampos(' abcdefghijklmnñopqrstuvwxyzáéiou');
--- content/validaciones_form.js
+++ content/validaciones_form.js
@@ -8,27 +8,25 @@
     validaciones_form = {
 
         /*validar correo electronico*/
-        validar_email : function (that) {
+        validar_email: function (that) {
 
 
-
-            if(this.isEmail($(that).val())==true){
+            if (this.isEmail($(that).val()) == true) {
                 //$(that).css({'background':'url("content/images/alert.png")'});
-                $(that).next().children('svg').css("display","none");
+                $(that).next().children('svg').css("display", "none");
                 $(that).siblings('div .tooltip').text('Tu correo esta Bien');
 
 
-            }else{
-               //no es email
-                $(that).next().children('svg').css("display","block");
+            } else {
+                //no es email
+                $(that).next().children('svg').css("display", "block");
                 $(that).siblings('div .tooltip').text('Esta mal tu correo');
 
             }
 
 
-
         },
-        isEmail : function (email) {
+        isEmail: function (email) {
             var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
             return regex.test(email);
         }
@@ -36,6 +34,33 @@
     }
 
 
-
 })
 (jQuery);
+
+
+(function (a) {
+    a.fn.validCampos = function (b) {
+        a(this).on({
+            keypress: function (a) {
+
+                var nombre_validacion = $(this).attr('id');
+                var jsonString = '{"'+nombre_validacion+'": "'+a.key+'"}';
+
+
+                var json = JSON.parse(jsonString);
+
+                console.log(json);
+                if(validate(json, validacion_) != undefined){
+                    console.log(validate(json, validacion_)[nombre_validacion][0]);
+                    $(this).parent('.validable').find('.tooltip').html(validate(json, validacion_)[nombre_validacion][0]);
+                    $(this).parent('.validable').find('.tooltip').css({"display":""});
+                }
+
+
+                var c = a.which, d = a.keyCode, e = String.fromCharCode(c).toLowerCase(), f = b;
+                (-1 != f.indexOf(e) || 9 == d || 37 != c && 37 == d || 39 == d && 39 != c || 8 == d || 46 == d && 46 != c) && 161 != c || a.preventDefault()
+            }
+        })
+    }
+})(jQuery);
+
